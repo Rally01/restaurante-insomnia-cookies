1290bc79ce202257764576596e33b882765565a2
fabicorrales91 <fabicorrales91@gmail.com>
2021-10-12 23:08:24 -0500

    post2

--- index.py
+++ index.py
@@ -1,33 +1,49 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 from flask.globals import request
 
 app = Flask(__name__)
 
-@app.route('/', methods = ('GET', 'POST'))
+
+@app.route('/') #Ruta de inicio, pagina principal donde inicia la web
 def inicio():
-
-    if(request.method=='POST'):    
-
-        if(request.form['correoingreso'] == True):
-            name='ingreso'
-            return name
-        else:
-            cad=""
-        
-            cad = request.form['correoregistro']
-            cad = cad + " - " + request.form['passwordregistro']
-
-            return cad
-
     return render_template('inicio.html')
 
-@app.route('/menu/')
+
+@app.route('/ingreso/', methods = ('GET', 'POST')) #Validacion desde el formulario de ingreso
+def ingreso():
+
+    if(request.method=='POST'):
+        cad=""
+        
+        cad = request.form['correoingreso']
+        cad = cad + " - " + request.form['passwordingreso']
+
+        return cad
+        #return redirect('/menu/')
+
+    #return render_template('menu.html')
+
+
+@app.route('/registro/', methods = ('GET', 'POST')) #Validacion desde el formulario de registro
+def registro():
+
+    if(request.method=='POST'):
+        cad=""
+        
+        cad = request.form['nombreregistro']
+        cad = cad + " - " + request.form['correoregistro']
+
+        return cad
+        #return redirect('/menu/')
+
+    #return render_template('menu.html')
+
+
+@app.route('/menu/') #Ruta a la pagina del menu
 def menu():
     return render_template('menu.html')
 
 
+
 if __name__ == '__main__':
-
- # Iniciamos la apicación en modo debug
-
- app.run(debug=True)
+ app.run()
